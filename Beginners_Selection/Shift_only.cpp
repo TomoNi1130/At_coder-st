ac7067f591f6c57098b188e63b3a73f288718c06
Tomoni11 <171126805+TomoNi1130@users.noreply.github.com>
2025-11-11 22:28:00 +0900

    四問目に模範解答を記入

--- Beginners_Selection/Shift_only.cpp
+++ Beginners_Selection/Shift_only.cpp
@@ -23,6 +23,22 @@ int main() {
     divided_nums.push_back(divided);
   }
   std::cout << int(*std::min_element(divided_nums.begin(), divided_nums.end())) << std::endl;
-
   return 0;
+
+  /*bit演算のみですると,,,（模範解答）
+
+  int N, input, buf = 0, ans = 0;
+  std::cin >> N;
+  for (int i = 0; i < N; i++) {
+    std::cin >> input;
+    buf = (input | buf);  // bit和
+  }
+  while (!(buf & 1)) {
+    buf = buf >> 1;
+    ans++;
+  }
+  std::cout << ans << std::endl;
+  return 0;
+
+  */
 }
