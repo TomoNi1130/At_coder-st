8c767db3dc7e05a08bf74411e26fc8c6039b721a
Tomoni11 <171126805+TomoNi1130@users.noreply.github.com>
2025-11-11 16:22:54 +0900

    initial commit

---
+++ Beginners_Selection/first.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int a, b, c;
+std::string s;
+
+int main() {
+  std::cin >> a;
+  std::cin >> b;
+  std::cin >> c;
+
+  std::cin >> s;
+
+  std::cout << (a + b + c) << " " << s << std::endl;
+
+  return 0;
+}
